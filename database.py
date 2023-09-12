e8282b607bcb069abe0532629098b33a212455cc
LeeHeonWoo1 <gjsdn245@naver.com>
2023-09-12 13:49:46 +0900

    format with black

--- database.py
+++ database.py
@@ -1,32 +1,42 @@
+"""
+데이터 베이스 연결을 관리하는 파일입니다.
+"""
+
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from starlette.config import Config
 
-config = Config('./.env')
+config = Config("./.env")
 
-SQLALCHEMY_DATABASE_URL = config('SQLALCHEMY_DATABASE_URL')
-if SQLALCHEMY_DATABASE_URL.startswith('sqlite'):
-    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread":False})
+SQLALCHEMY_DATABASE_URL = config("SQLALCHEMY_DATABASE_URL")
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
 else:
     engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 Base = declarative_base()
 naming_convention = {
-    "ix": 'ix_%(column_0_label)s',
+    "ix": "ix_%(column_0_label)s",
     "uq": "uq_%(table_name)s_%(column_0_name)s",
     "ck": "ck_%(table_name)s_%(column_0_name)s",
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
+    "pk": "pk_%(table_name)s",
 }
 Base.metadata = MetaData(naming_convention=naming_convention)
 # meta = Base.metadata
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+
 def get_db():
-    db = SessionLocal()
+    """
+    Local 환경에서 데이터베이스를 연결하여 db를 생성하는 generator입니다.
+    """
+    database = SessionLocal()
     try:
-        yield db
+        yield database
     finally:
-        db.close()
+        database.close()
